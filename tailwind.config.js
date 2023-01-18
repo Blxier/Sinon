4ceba7e232fd1df3e5961dc714d488e1adfda3e2
JR_RUEL <blxier.odaroma@gmail.com>
2023-01-18 19:16:38 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+      Roboto: ["Roboto Condensed, sans-serif"],
+    },
+    colors:{
+      "white": "#FFFFFF",
+      "black": "#000000",
+      "white-ish": "gray",
+      "main-color": "#006E68",
+      "secondary-color": "#03211F",
+      "gray":"gray",
+      
+    
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      
+    }
+  
+  },
+  plugins: [],
+}
